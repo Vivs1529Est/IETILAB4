9c51971fb392d09d58a0e76f9b456228b336aa3a
estudiante <estudiante@linux-57.labinfo.is.escuelaing.edu.co>
2019-02-20 13:27:57 -0500

    login1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoList} from "./TodoList";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from "moment";
+import {Login} from "./component/Login";
+import {TodoApp} from "./TodoApp";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+
+
+
+const LoginView = () => (
+    <Login/>
+);
+
+const TodoView = () => (
+    <TodoApp/>
+);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+       super(props);
+       localStorage.setItem('email',"cosw");
+       localStorage.setItem('password',"123");
+
+    }
+
+    render() {
+
+            return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+
+                    <br/>
+                    <br/>
+
+                    <ul>
+                        {localStorage.getItem('isLoggedIn') === "true"?
+                            <li><Link to="/todo">Todo</Link></li> : <li><Link to="/">Login</Link></li>}
+                    </ul>
+
+                    <div>
+                        {localStorage.getItem('isLoggedIn') === "true"?
+                            < Route path = "/todo" component={TodoView}/> : <Route exact path="/" component={LoginView}/>
+                        }
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+
 }
 
 export default App;
